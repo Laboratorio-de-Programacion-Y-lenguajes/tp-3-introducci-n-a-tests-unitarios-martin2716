74d36acf1e81f9f86b65ad4b0a153aa2c3ecc15e
Martin Daniel Acosta <martindanielacosta@MacBook-Pro-de-Raul.local>
2026-04-21 20:21:06 -0300

    Completar  tests en tests/test_pow.py

--- tests/test_mul.py
+++ tests/test_mul.py
@@ -14,9 +14,39 @@ def test_mul_positivos():
 # --- TU TURNO ---
 # Agregá tests para los siguientes casos:
 #   - Multiplicar por cero
+
+def test_mul_cero():
+    """Ejemplo: 3 * 0 debe dar 0."""
+    assert mul(3, 0) == 0
+
+
 #   - Multiplicar dos números negativos (resultado positivo)
+
+def test_mul_negativos():
+    """Ejemplo: -3 * -2 debe dar 6."""
+    assert mul(-3, -2) == 6
+
+
 #   - Multiplicar un positivo y un negativo (resultado negativo)
+
+def test_mul_negativoypositivo():
+    """Ejemplo: -3 * 2 debe dar -6."""
+    assert mul(-3, 2) == -6
+
+
 #   - Multiplicar por 1 (elemento neutro)
+
+def test_mul_neutro():
+    """Ejemplo: -3 * 1 debe dar -3."""
+    assert mul(-3, 1) == -3
+
+
 #   - Multiplicar dos decimales (float)
+
+def test_mul_decimales():
+    """Ejemplo: 3.2 * 2.1 debe dar 6.72."""
+    assert mul(3.02 , 2.01) == 6.0702
+
+
 #
 # Pista: podés usar @pytest.mark.parametrize para probar varios casos a la vez.
