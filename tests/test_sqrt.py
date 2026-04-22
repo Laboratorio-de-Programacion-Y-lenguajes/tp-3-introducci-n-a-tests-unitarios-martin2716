f9db5952d0ea814380c4d4b0fac08ed462c5c99e
Martin Daniel Acosta <martindanielacosta@MacBook-Pro-de-Raul.local>
2026-04-21 21:01:59 -0300

    Completar  tests en tests/test_sub.py

--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -14,8 +14,25 @@ def test_sqrt_cuadrado_perfecto():
 # --- TU TURNO ---
 # Agregá tests para los siguientes casos:
 #   - Raíz de 0 (resultado: 0.0)
+
+def test_sqrt_raiz_cero():
+    """Ejemplo: la raíz de 0 debe dar 0."""
+    assert sqrt(0) == 0.0
+
+
 #   - Raíz de un número que no es cuadrado perfecto (resultado decimal)
+
+def test_sqrt_NO_cuadrado_perfecto():
+    """Ejemplo: la raíz de 11 debe dar 3.3166."""
+    assert sqrt(11) == pytest.approx(3.3166, rel=1e-4)
+
+
 #   - Raíz de un número negativo → debe lanzar ValueError
+
+def test_sqrt_negativo():
+     with pytest.raises(ValueError):
+         sqrt(-4)
+
 #
 # Pista: para testear excepciones usá pytest.raises:
 #
